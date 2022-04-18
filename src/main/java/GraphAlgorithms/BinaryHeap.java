d429172d1b5f2ca37b96a71fab424f01b6c3aaa6
Bruno Preka <bruno.preka@imt-atlantique.net>
2022-04-18 16:53:04 +0200

    [BinaryHeap] add main operations

--- src/main/java/GraphAlgorithms/BinaryHeap.java
+++ src/main/java/GraphAlgorithms/BinaryHeap.java
@@ -1,6 +1,8 @@
 package GraphAlgorithms;
 
 
+import java.util.Arrays;
+
 public class BinaryHeap {
 
     private int[] nodes;
@@ -14,7 +16,7 @@ public class BinaryHeap {
         this.pos = 0;
     }
 
-    public void resize() {
+    public void resize() { // linear complexity
         int[] tab = new int[this.nodes.length + 32];
         for (int i = 0; i < nodes.length; i++) {
             tab[i] = Integer.MAX_VALUE;
@@ -27,36 +29,106 @@ public class BinaryHeap {
         return pos == 0;
     }
 
+
+    /**
+     * Inserts an element into the heap
+     * @implNote Complexity: O(log_2(this.pos)) (check the detail in the code comments)
+     *
+     */
     public void insert(int element) {
-    	// TODO: Complete
-    }
 
-    public int remove() {
-    	// TODO: Complete
-    	return 0;
-    }
-
-    private int getBestChildPos(int src) {
-        if (isLeaf(src)) { // the leaf is a stopping case, then we return a default value
-            return Integer.MAX_VALUE;
-        } else {
-        	// TODO: Complete
-        	return Integer.MAX_VALUE;
+        // if this tree is full, it must be resized
+        if (this.pos == this.nodes.length){
+            this.resize(); // complexity in O(this.pos)
         }
+
+
+        // 1. Add `element` to the first available leaf
+        // append `element` at the end of the array
+        int i = this.pos;
+        this.pos++;
+        this.nodes[i] = element;
+
+        // 2. Percolate-up if needed
+        while (i != 0 && this.nodes[this.getParentIndex(i)] > this.nodes[i]) { // worst case in complexity: O(log_2(this.pos)), since a heap is a binary tree
+            this.swap(i, this.getParentIndex(i)); // complexity in O(1)
+            i = this.getParentIndex(i); // same as well
+        }
+
+    }
+
+    /**
+     * Removes the smallest value of the heap
+     * @implNote Complexity: O(log_2(this.pos)) (check the detail in the code comments)
+     *
+     */
+    public int remove() {
+    	return this.removeMin();
+    }
+
+    /**
+     * Removes the smallest value of the heap
+     * @implNote Complexity: O(log_2(this.pos)) (check the detail in the code comments)
+     *
+     */
+    public int removeMin(){
+        if (isEmpty())
+            return Integer.MAX_VALUE;
+
+        // 1. swap root and last-used leaf (i.e. the (this.pos-1)-th element in the array)
+        this.nodes[0] = this.nodes[this.pos-1];
+        this.nodes[this.pos-1] = Integer.MAX_VALUE;
+        this.pos--;
+        int i = 0;
+
+        // 2. swap downwardly (percolate-down)
+        while (!isLeaf(i)){ // navigating downwards is done in O(log_2(this.pos)), since a heap is a binary tree
+            if (this.nodes[i] > this.nodes[2*i+1] || this.nodes[i] > this.nodes[2*i+2]){
+                /* in order to keep the binary heap's order property true,
+                    picking the smallest child is mandatory */
+                if (this.nodes[2*i+1] < this.nodes[2*i+2]){
+                    this.swap(i,2*i+1);
+                    i = 2*i+1;
+                } else {
+                    this.swap(i,2*i+2);
+                    i = 2*i+2;
+                }
+            }
+        }
+        return i;
+    }
+
+
+    /**
+     * Get a child's parent index
+     *
+     * @returns the parent's index
+     *
+     */
+    private int getParentIndex(int childIndex){
+        return childIndex != 0 ? (childIndex-1)/2 : 0; // quotient floor-rounded by default (between integers)
     }
 
     
     /**
 	 * Test if the node is a leaf in the binary heap
-	 * 
+	 *
+     * @param src is the index of the node in the array
 	 * @returns true if it's a leaf or false else
 	 * 
 	 */	
     private boolean isLeaf(int src) {
-    	// TODO: Complete
-    	return false;
+        return 2* src +1 >= this.pos && 2* src +2 >= this.pos; // if no children, then it's a leaf
     }
 
+    /**
+     * Swaps given indexes of a father and one of its child
+     *
+     * @param father
+     * @param child
+     * @implNote Child becomes father, and inversely
+     *
+     */
     private void swap(int father, int child) {
         int temp = nodes[father];
         nodes[father] = nodes[child];
@@ -87,30 +159,54 @@ public class BinaryHeap {
         } else {
             int left = 2 * root + 1;
             int right = 2 * root + 2;
-            if (right >= pos) {
+            if (right >= pos) { // if no right child node -> check left side
                 return nodes[left] >= nodes[root] && testRec(left);
-            } else {
+            } else { // check both sides
                 return nodes[left] >= nodes[root] && testRec(left) && nodes[right] >= nodes[root] && testRec(right);
             }
         }
     }
 
     public static void main(String[] args) {
+        // init a random binary heap
         BinaryHeap jarjarBin = new BinaryHeap();
-        System.out.println(jarjarBin.isEmpty()+"\n");
+        System.out.println("is it empty: " + jarjarBin.isEmpty()+"\n");
         int k = 20;
         int m = k;
         int min = 2;
         int max = 20;
+
+        // fill and print inserted values
         while (k > 0) {
             int rand = min + (int) (Math.random() * ((max - min) + 1));
-            System.out.print("insert " + rand);
+            System.out.print("insert " + rand + "  ");
             jarjarBin.insert(rand);            
             k--;
         }
-     // TODO: Complete
+
+        // print the heap's array
         System.out.println("\n" + jarjarBin);
-        System.out.println(jarjarBin.test());
+        //System.out.println("this.nodes: " + Arrays.toString(jarjarBin.nodes)); // debug: print the whole array
+
+        // test if the heap's valid
+        System.out.println("Is the heap valid : " + jarjarBin.test());
+
+        // remove the min value
+        System.out.println();
+        System.out.println("\n--- Remove the min value ---");
+        int indexLastElem = jarjarBin.removeMin();
+        System.out.println("indexLastElem = " + indexLastElem);
+        System.out.println("\n" + jarjarBin);
+        System.out.println("Is the heap valid : " + jarjarBin.test());
+
+        // MOCK: force append negative infinity to test heap's validity
+        System.out.println("\nmock test");
+        jarjarBin.nodes[jarjarBin.pos] = Integer.MIN_VALUE;
+        jarjarBin.pos++;
+        System.out.println("pos = " + jarjarBin.pos);
+        System.out.println("parent = " +jarjarBin.nodes[jarjarBin.getParentIndex(jarjarBin.pos-1)]);
+        System.out.println("\n" + jarjarBin);
+        System.out.println("Is the heap valid : " + jarjarBin.test());
     }
 
 }
