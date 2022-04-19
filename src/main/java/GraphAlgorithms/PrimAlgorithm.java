fc72b5bb769f41fb6f36b0df5facabcacfb540e0
Bruno Preka <bruno.preka@imt-atlantique.net>
2022-04-19 22:23:41 +0200

    begin Prim's algorithm

---
+++ src/main/java/GraphAlgorithms/PrimAlgorithm.java
@@ -0,0 +1,10 @@
+package GraphAlgorithms;
+
+public class PrimAlgorithm {
+    /* This is a class dedicated to Prim's algorithm using:
+        * a binary heap storing non-visited-yet vertices's minimum weight (among the adjacent vertices)
+        * an undirected valued graph (the adjacency list implementaion is used here) which represents the graph
+     */
+
+
+}
